df39cb5a7af32ef030c2b135515c71f8335717f3
Ashrith K S <ashks@cisco.com>
2018-09-19 11:11:08 +0530

    version change

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import { Platform } from 'react-native';
 
 export default function testID(id) {
+  if(__DEV__){
+    console.log(`testId is : ${id}`)
+  }
   return Platform.OS === 'android' ?
     { accessible: true, accessibilityLabel: id } :
     { testID: id };

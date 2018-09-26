7378297244ff1216f2a4be361352408833e7a5ed
Ashrith K S <ashks@cisco.com>
2018-09-26 11:48:09 +0530

    removed platform

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-import { Platform } from 'react-native';
 
 export default function testID(id, isConsoleRequired = false) {
   if (__DEV__ && isConsoleRequired) {
     console.log(`testId is : ${id}`)
   }
-  return Platform.OS === 'android' ?
-    { accessible: true, accessibilityLabel: id } :
-    { testID: id };
+  return { accessible: true, accessibilityLabel: id, testID: id }
 }

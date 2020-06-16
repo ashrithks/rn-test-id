eaf26fcfc4ad6cfea3b9a23dea47c27721a8ac69
Ashrith K S <ashks@cisco.com>
2020-06-16 10:38:04 +0530

    added accesible bool

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 
-export default function testID(id, isConsoleRequired = false) {
+export default function testID(id, accessible = false, isConsoleRequired = false) {
   if (__DEV__ && isConsoleRequired) {
     console.log(`testId is : ${id}`)
   }
-  if (__DEV__) {
+  if (__DEV__ && accessible) {
     return { accessible: true, accessibilityLabel: id, testID: id }
+  }
+  else if (__DEV__) {
+    return { testID: id }
   }
   return {}
 }
